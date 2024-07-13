a3b1fbfcc9f2e0092a98bc9862ff20a4eb14d338
Juanpistolo <99058581+Juanpistolo@users.noreply.github.com>
2024-07-13 16:19:22 -0500

    up

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import $ from "jquery";
+import React, { Component, createRef } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+window.jQuery = $;
+window.$ = $;
+
+require("jquery-ui-sortable");
+require("formBuilder");
+
+const formData = [
+  {
+    type: "header",
+    subtype: "h1",
+    label: "formBuilder in React"
+  },
+  {
+    type: "paragraph",
+    label: "This is a demonstration of formBuilder running in a React project."
+  }
+];
+
+class FormBuilder extends Component {
+  fb = createRef();
+  componentDidMount() {
+    $(this.fb.current).formBuilder({ formData });
+  }
+
+  render() {
+    return (
+      <div className="bg-gray-100 p-4">
+      <div className="bg-white p-4 rounded-md shadow-md">
+        <div className="alert alert-info mb-4">
+          Cree la plantilla
+        </div>
+        <div id="fb-editor" ref={this.fb} />
+      </div>
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(<FormBuilder />, document.getElementById("root"));
